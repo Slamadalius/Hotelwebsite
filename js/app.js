547db95809b55c629e4bb1a4a8253a2ad2531433
Slamadalius <slamasdalius@gmail.com>
2016-12-01 15:42:39 +0000

    adding single pages

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
+function getHeight() {
+	var height = $('.promo, .promo-video').height();
+	$('#overlay').css({'height': height + 'px'})
+}
+
 $(function () {
+	window.setTimeout('getHeight()',100);
 	$('#gallery a').on('click', function () {
 		var image = $(this).find('img').attr('src');
 		var imageSelected = image.split('thumbnail');
@@ -7,5 +13,9 @@ $(function () {
 		$('#galleryModal #imageContainer').attr('src', fullPath);
 	})
 })
+
+$(window).resize(function() {
+	getHeight();
+});
 
 $(document).foundation();
